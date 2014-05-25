41acc51fbc76c6d31b0b02d378ed8f391848a583
grozadilla <gontzal.rozadilla@gmail.com>
2014-05-25 14:30:59 +0100

    últimos cambios con rutas

--- MapasPorElMundo/GRUViewController.m
+++ MapasPorElMundo/GRUViewController.m
@@ -11,6 +11,8 @@
 @interface GRUViewController (){
     CLLocationManager  * locationManager;
     CLLocation * location;
+    
+    id<MKOverlay> rutaOverlay;
 }
 @end
 
@@ -42,33 +44,80 @@
 }
 
 -(void)tapPosition:(UITapGestureRecognizer *) gesture{
+    
+    //detectamos el numero de toques
+    //1. geocoding
+    //2. rutas
+    
     CGPoint point = [gesture locationInView:self.mapView];
     
     CLLocationCoordinate2D coordinates = [self.mapView convertPoint:point toCoordinateFromView:self.mapView];
     
-    location = [[CLLocation alloc] initWithLatitude:coordinates.latitude longitude:coordinates.longitude];
+    switch (gesture.numberOfTouches) {
+        case 0:{
+            CLGeocoder *geocoder = [[CLGeocoder alloc]init];
+            [geocoder reverseGeocodeLocation:location completionHandler:^(NSArray *placemarks, NSError *error){
+                if (!error) {
+                    CLPlacemark * placeMark = [placemarks objectAtIndex:0];
+                    NSLog(@"%@",placeMark.addressDictionary);
+                }
+            }];
+        }
+        break;
+        case 1:{
+            //calculamos rutas
+            
+            //tenemos que configurar el request para calcular la ruta, necsitamoslas coordenaadas de inicia  fin + tipo de transporte
+            MKDirectionsRequest * request = [[MKDirectionsRequest alloc]init];
+            request.transportType = MKDirectionsTransportTypeWalking;
+            
+            request.destination = [[MKMapItem alloc]initWithPlacemark:[[MKPlacemark alloc]initWithCoordinate: coordinates addressDictionary:nil]];
+            
+            request.source = [[MKMapItem alloc]initWithPlacemark:[[MKPlacemark alloc]initWithCoordinate:location.coordinate addressDictionary:nil]];
+            
+            MKDirections * directions = [[MKDirections alloc]initWithRequest:request];
+            [directions calculateDirectionsWithCompletionHandler:^(MKDirectionsResponse *response, NSError *error) {
+                if (!error) {
+                    MKRoute *route = response.routes[0];
+                    [self drawRoute:route];
+                }
+            }];
+            
+            
+        }
+        break;
+        default:
+            break;
+    
+    
+    }
+    
+    /*location = [[CLLocation alloc] initWithLatitude:coordinates.latitude longitude:coordinates.longitude];
     
     MKCoordinateRegion regionCurrentUser;
     regionCurrentUser.center.latitude = location.coordinate.latitude;
     regionCurrentUser.center.longitude = location.coordinate.longitude;
     
-    [self.mapView setRegion:regionCurrentUser animated:YES];
-    
-    //geolocalizacion de las coordenadas al cp, calle, zona ... (no siempre se obtiene todo de todos los pntos)
-    
-    CLGeocoder *geocoder = [[CLGeocoder alloc]init];
-    [geocoder reverseGeocodeLocation:location completionHandler:^(NSArray *placemarks, NSError *error){
-        if (!error) {
-            CLPlacemark * placeMark = [placemarks objectAtIndex:0];
-            NSLog(@"%@",placeMark.addressDictionary);
-        }
-    }];
+    [self.mapView setRegion:regionCurrentUser animated:YES];*/
     
     
+}
+
+-(void)drawRoute:(MKRoute *) route{
+    if (rutaOverlay) {//si ya hay una ruta la elimino
+        [self.mapView removeOverlay:rutaOverlay];
+    }
+    [self.mapView addOverlay:route.polyline];
+}
+
+-(MKOverlayRenderer *)mapView:(MKMapView *)mapView rendererForOverlay:(id<MKOverlay>)overlay{
+    rutaOverlay = overlay;
+    MKPolylineRenderer *renderLine = [[MKPolylineRenderer alloc]initWithOverlay:overlay];
     
+    renderLine.strokeColor = [UIColor blueColor];
+    renderLine.lineWidth = 3.0;
     
-    
-    
+    return renderLine;
 }
 
 - (void)didReceiveMemoryWarning
@@ -137,7 +186,7 @@
 - (void)mapView:(MKMapView *)mapView didUpdateUserLocation:(MKUserLocation *)userLocation{
     
     //es un buen stio para detener la localizacion
-    //[locationManager stopUpdatingLocation];
+    [locationManager stopUpdatingLocation];
     
     //actualizar el mapa
     
